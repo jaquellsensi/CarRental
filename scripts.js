39800080cdf9ca556b0c905af00df9af4707aa9c
jaquellsensi <146892715+jaquellsensi@users.noreply.github.com>
2023-10-26 16:02:11 -0700

    not looking so good slow start

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+window.onload = function() {
+   const totalButton = document.getElementById("totalButton");
+   totalButton.addEventListener("click", total);
+}
+
+function total() {  return  totalButton.innerHTML; }
+
+
+
